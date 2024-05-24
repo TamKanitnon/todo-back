e2b417c87a2b17c61b22e1c1b8c421d2988a9b3d
TamKanitnon <TamKanitnon@hotmail.com>
2024-05-24 15:30:45 +0700

    made https

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
-
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/cert.key'),
+  cert: fs.readFileSync('./secrets/cert.crt'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: ["http://127.0.0.1:4200"] });
+  app.enableCors({ origin: ["http://127.0.0.1:4200", "http://147.50.231.83"] });
   await app.listen(3000);
 }
 bootstrap();
