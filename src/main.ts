0992ffbde22393c3cbb184e63217e775f9d6bc37
TamKanitnon <TamKanitnon@hotmail.com>
2024-05-24 15:28:59 +0700

    todo version 1.5

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as fs from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: ['http://147.50.231.83', 'http://127.0.0.1:4200'] });
+  app.enableCors({ origin: ["http://127.0.0.1:4200"] });
   await app.listen(3000);
 }
 bootstrap();
